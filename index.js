4347e22ea2084017314669e3c002bf4824698b48
BackloMabele <backlo.mabele@student.moringaschool.com>
2023-09-24 21:57:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const nameToLowerText = name.toLowerCase();
+    const matchingDrivers = drivers.filter(driver => {
+        const driverLower = driver.toLowerCase();
+        return driverLower.includes(nameToLowerText);
+    });
+    if (matchingDrivers.length === 0) {
+        return [];
+    }
+    return matchingDrivers;
+    }
+    function fuzzyMatch(drivers, name) {
+        const searchToLowerCase = name.toLowerCase()
+        const matchingDrivers = drivers.filter(driver => {
+            const driverLower = driver.toLowerCase();
+            return driverLower.startsWith(searchToLowerCase);
+        })
+        return matchingDrivers;
+        }
+    function matchName(drivers, name) {
+        const matchingString = drivers.filter(driver => driver.name === name);
+        return matchingString;
+        }
